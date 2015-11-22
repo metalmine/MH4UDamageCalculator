ff99e8e20f6cdf4cdb13b7adc3c35e942b36c62a
Xiaohan Ji <lightenedblade@gmail.com>
2015-11-21 22:21:36 -0800

    Added the site

---
+++ MH4U Damage calc/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+//at start of document
+	$("div.selectionPop").hide();
+	$("div.pressA").hide();
+	$(".weaponIcon").hide();
+	$(".monsterIcon").hide();
+	$(".buffIcon").hide();
+//Within Weapon and Armor Skill Selection
+	$(".weaponCell").click( function(){
+		$("div.selectionPop").hide("slide",500);
+		$("div.pressA").hide("slide",500);
+		$(".weaponIcon").attr("id", $(this).attr('id'));
+	});
+	$(".weaponIcon").click( function(){
+		$("div.selectionPop").show("slide",500);
+		$("div.pressA").show("slide",500);
+	});
+});
