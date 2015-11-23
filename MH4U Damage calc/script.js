8fd92f4977776c967a972eb9881c32b2bc992424
Xiaohan Ji <lightenedblade@gmail.com>
2015-11-22 17:19:15 -0800

    v0.001
    - Created onSelect class for weaponIcon

--- MH4U Damage calc/script.js
+++ MH4U Damage calc/script.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
-/*at start of document
-	$("div.selectionPop").hide();
-	$("div.pressA").hide();
+//at start of document
+$("div.selectionPop").hide();
+/* 	$("div.pressA").hide();
 	$(".weaponIcon").hide();
 	$(".monsterIcon").hide();
-	$(".buffIcon").hide();*/
+	$(".buffIcon").hide(); */
 //Within Weapon and Armor Skill Selection
 	$(".weaponCell").click( function(){
 		$("div.selectionPop").hide("slide",500);
 		$("div.pressA").hide("slide",500);
 		$(".weaponIcon").attr("id", $(this).attr('id'));
+		$("div.weaponIcon").removeclass("onSelect").addclass("onDeselect");
 	});
 	$(".weaponIcon").click( function(){
 		$("div.selectionPop").show("slide",500);
 		$("div.pressA").show("slide",500);
+		$("div.weaponIcon").addclass("onSelect").removeclass("onDeselect");
 	});
+	
+//Within Buff Selection
+
+
+//Within Monster Selection
+	
 });
