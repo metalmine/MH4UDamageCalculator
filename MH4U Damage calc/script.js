43e6a85f967d216eb93f66ba91512a71230fe6ee
Xiaohan Ji <lightenedblade@gmail.com>
2015-11-23 21:17:09 -0800

    v0.08
    can select monsters now!

--- MH4U Damage calc/script.js
+++ MH4U Damage calc/script.js
@@ -1,12 +1,37 @@
 //Tracks if the background buttons should be disabled
 var disableBackground = false;
 
+//Tracks selected options
+var selectedMonsterID;
+var selectedWeaponID;
+
 $(document).ready(function(){
-//at start of document
-$("div.selectionPop").hide();
-/* 	$("div.pressA").hide(); */
-//Within Weapon and Armor Skill Selection
+	//at start of document
+	$("div.selectionPop").hide();
+	/* 	$("div.pressA").hide(); */
 	
+
+	//Populate Monster Selection
+	var htmlString = "<select id=\"monsterSelect\">\n<option value=\"\">Please Select a Monster</option>\n";
+	for( var i = 0; i < monstersSelectionList.length; i++ ){
+		htmlString += "<option value=\"" + monstersSelectionList[i]["sort_name"] + "\">" + monstersSelectionList[i]["name"] + "</option>\n";
+	}
+	htmlString += "</select>";
+	$(".monsterPick").html( htmlString );
+	
+	//Callback when monster is selected
+	 $("#monsterSelect").change( function(value){
+	  var val = $(this).val();
+	  if( !val || 0 === val.length ) return;
+	  
+	  selectedMonsterID = monstersSelectionList.filter( function( obj ){
+	   return obj[ "sort_name" ] == val;
+	  })[0].id;
+	  $(".monsterIcon").attr( "id", val );
+	 });
+
+
+	//Within Weapon and Armor Skill Selection
 	$(".weaponCell").click( function(){
 		$(".weaponIcon").attr("id", $(this).attr('id'));
 	});
@@ -21,10 +46,10 @@ $("div.selectionPop").hide();
 		}
 	});
 	
-//Within Buff Selection
+	//Within Buff Selection
 
 
-//Within Monster Selection
+	//Within Monster Selection
 	$(".monsterIcon").click( function(){
 		if( !disableBackground ){
 			$("div.monPop").show("slide",500);
@@ -36,7 +61,7 @@ $("div.selectionPop").hide();
 
 	});
 	
-//Done button
+	//Done button
 	$(".buttonDone").click( function(){
 		$("div.wepPop").hide("slide",500);
 		$("div.monPop").hide("slide",500);
