58f2b6ded010f1fbdeb9958336b10b7b6a2bc8c8
Xiaohan Ji <lightenedblade@gmail.com>
2015-11-24 00:09:12 -0800

    v0.10
    you can pick weapons  now!

--- MH4U Damage calc/script.js
+++ MH4U Damage calc/script.js
@@ -4,6 +4,7 @@ var disableBackground = false;
 //Tracks selected options
 var selectedMonsterID;
 var selectedWeaponID;
+var selectedRank = 1;
 
 $(document).ready(function(){
 	//at start of document
@@ -20,21 +21,76 @@ $(document).ready(function(){
 	$(".monsterPick").html( htmlString );
 	
 	//Callback when monster is selected
-	 $("#monsterSelect").change( function(value){
-	  var val = $(this).val();
-	  if( !val || 0 === val.length ) return;
-	  
-	  selectedMonsterID = monstersSelectionList.filter( function( obj ){
-	   return obj[ "sort_name" ] == val;
-	  })[0].id;
-	  $(".monsterIcon").attr( "id", val );
-	 });
+	$("#monsterSelect").change( function(){
+		var val = $(this).val();
+		if( !val || 0 === val.length ) return;
+		
+		selectedMonsterID = monstersSelectionList.filter( function( obj ){
+			return obj[ "sort_name" ] == val;
+		})[0].id;
+		$(".monsterIcon").attr( "id", val );
+	});
+	
+	//Callback when rank is selected
+	$("#rankSelect").change( function( value ){
+		selectedRank = $(this).val();
+	});
 
 
 	//Within Weapon and Armor Skill Selection
 	$(".weaponCell").click( function(){
 		$(".weaponIcon").attr("id", $(this).attr('id'));
+		
+		//Set Weapon
+		var weaponClass = 0;
+		switch( $(this).attr( "id" ) ){
+			case "gs":
+				weaponClass = 1;
+				break;
+			case "ls":
+				weaponClass = 2;
+				break;
+			case "db":
+				weaponClass = 4;
+				break;
+			case "sns":
+				weaponClass = 3;
+				break;
+			case "ham":
+				weaponClass = 5;
+				break;
+			case "hh":
+				weaponClass = 6;
+				break;
+			case "la":
+				weaponClass = 7;
+				break;
+			case "gla":
+				weaponClass = 8;
+				break;
+			case "sa":
+				weaponClass = 9;
+				break;
+			case "cb":
+				weaponClass = 10;
+				break;
+			case "ig":
+				weaponClass = 11;
+				break;
+		}
+		var filteredWeapons = weaponList.filter( function( obj ){
+				return obj.class == weaponClass;
+			});
+		var htmlString = "<select id=\"wepSel\" onchange=\"setWeapon(this)\">\n<option value=\"\">Please Select a Weapon</option>\n";
+		for( var i = 0; i < filteredWeapons.length; i++ ){
+			htmlString += "<option value=\"" + filteredWeapons[i].id + "\">" + filteredWeapons[i].name + "</option>\n";
+		}
+		htmlString += "</select>";
+		
+		$(".wepPick").html( htmlString );
 	});
+
+	
 	
 	$(".weaponIcon").click( function(){
 		if( !disableBackground ){
@@ -58,7 +114,6 @@ $(document).ready(function(){
 			
 			disableBackground = true;
 		}
-
 	});
 	
 	//Done button
@@ -67,10 +122,21 @@ $(document).ready(function(){
 		$("div.monPop").hide("slide",500);
 		$("div.buffPop").hide("slide",500);
 		$("div.monPop").hide("slide",500);
-		$("div.pressA").hide("slide",2000);
+		$("div.pressA").hide("slide", 0 );
 		$("div.weaponIcon").addClass("onSelect").removeClass("onDeselect");
 		$("div.monsterIcon").addClass("onSelect").removeClass("onDeselect");
 		
 		disableBackground = false;
 	});
 });
+
+
+function setWeapon( value ){
+	
+	var val = value.value;
+	
+	if( !val || 0 === val.length || val < 0 ) return;
+	
+	selectedWeaponID = val;
+	
+}
