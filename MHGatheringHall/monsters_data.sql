dbf435e5eacff132ac40da5341e03299091dea73
Xiaohan Ji <lightenedblade@gmail.com>
2016-04-18 19:34:54 -0700

    v999.0

---
+++ MHGatheringHall/monsters_data.sql
@@ -0,0 +1,6 @@
+/*
+Testing SQL.
+*/
+CREATE TABLE monster_data (id INTEGER PRIMARY KEY,name TEXT,health_low INTEGER,health_high INTEGER,health_G INTEGER);s
+INSERT INTO monsters_data VALUES (1,"Great Jaggi",320,400,800);
+SELECT * FROM monsters_data;
